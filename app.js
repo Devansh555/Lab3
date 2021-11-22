8dda27d3f558c0ef0e84b4187d939ff40978024f
Devansh Bhatt <db2017@hw.ac.uk>
2021-11-22 16:55:45 +0400

    Completed until dbQuery.js

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 //creating app
 const app = express();
-//send the index.html when receiving HTTP GET /
+//handling static HTML and EJS templates
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+//route for home
 app.get('/', (req, res) => {
- res.sendFile('index.html', { root: __dirname });
+ res.render('index'); //no need for ejs extension
 });
 
+//route for contacts
+app.get("/contacts", (req, res) => {
+    res.render("contacts");
+  });
+
+//route for login
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//route for register
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
