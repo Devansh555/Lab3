881070509099a2c74218ed920c3e707df1f8cea2
Devansh Bhatt <db2017@hw.ac.uk>
2021-11-23 01:45:32 +0400

    Made more changes

--- db/dbQuery.js
+++ db/dbQuery.js
@@ -11,33 +11,33 @@ var pool = mysql.createPool({
 });
 
 function executeQuery(query, callback) {
-  pool.getConnection(function (err, connection) {
-    if (err) {
-      return callback(err, null);
-    } else if (connection) {
-      connection.query(query, function (err, rows, fields) {
-        connection.release();
-        if (err) {
+  pool.getConnection(function(err, connection) {
+      if (err) {
           return callback(err, null);
-        }
-        return callback(null, rows);
-      });
-    } else {
-      return callback(true, "No Connection");
-    }
+      } else if (connection) {
+          connection.query(query, function(err, rows, fields) {
+              connection.release();
+              if (err) {
+                  return callback(err, null);
+              }
+              return callback(null, rows);
+          });
+      } else {
+          return callback(true, "No Connection");
+      }
   });
 }
 
 function getResult(query, callback) {
-  executeQuery(query, function (err, rows) {
-    if (!err) {
-      callback(null, rows);
-    } else {
-      callback(true, err);
-    }
+  executeQuery(query, function(err, rows) {
+      if (!err) {
+          callback(null, rows);
+      } else {
+          callback(true, err);
+      }
   });
 }
 
 module.exports = {
-  getResult,
+  getResult
 };
