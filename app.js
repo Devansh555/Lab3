8bf60a3bf7ef04108a4d57b8b97bf55225eb4380
Devansh Bhatt <db2017@hw.ac.uk>
2021-11-23 00:23:42 +0400

    made few changes

--- app.js
+++ app.js
@@ -1,51 +1,42 @@
 const express = require('express');
-const session = require("express-session");
+const session = require('express-session');
+
 //creating app
 const app = express();
+
 //handling static HTML and EJS templates
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
-//route for home
 app.get('/', (req, res) => {
  res.render('index'); //no need for ejs extension
 });
 
-// The login and register part
+//login and registeration segment
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
-  secret: 'test',
-  resave: true,
-  saveUninitialized: true
+    secret: 'test',
+    resave: true,
+    saveUninitialized: true
 }));
 app.get("/login", (req, res) => {
-  res.render("login");
+    res.render("login");
 });
+  
 app.get("/register", (req, res) => {
-
     res.render("register");
-
 });
+
 
 //route for contacts
-app.get("/contacts", (req, res) => {
-    res.render("contacts");
-  });
-
-//route for login
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-
-//route for register
-app.get("/register", (req, res) => {
-  res.render("register");
-});
+app.get('/contacts', (req, res) => {
+    res.render('contacts');
+   });
 
 //pass requests to the router middleware
 const router = require('./routes/apis');
 app.use(router);
+
 
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
